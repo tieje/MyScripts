ed2d1c3bf15c3945b014aad2aaec84c405ba1d03
Thomas Francis <toj320@gmail.com>
2020-08-18 00:10:31 -0400

    got as close to changing the directory as possible with python

--- myscripts.py
+++ myscripts.py
@@ -1,5 +1,21 @@
 import os
+import pyperclip
 
 class MyScripts:
-
+    def __init__(self, home, work):
+        self.home = home
+        self.work = work
+    def ChangeDir(self):
+        message = "Immediately paste into the command line."
+        try:
+            os.chdir(self.work)
+            pyperclip.copy('cd ' + self.work)
+        except:
+            os.chdir(self.home)
+            pyperclip.copy('cd ' + self.home)
+        print(message)
+        return None
 if __name__ == "__main__":
+    home = r"C:\MyScripts"
+    work = r"C:\CustomOfficeTemplates\current_code\MyScripts"
+    MyScripts(home,work).ChangeDir()
