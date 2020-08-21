47b0d746a2c984cefad172a72564631d4c371bf6
Thomas Francis <toj320@gmail.com>
2020-08-21 00:35:04 -0400

    learning about subprocess module

--- runsqlscripts.py
+++ runsqlscripts.py
@@ -1,6 +1,22 @@
 import os
+import subprocess
 
 
+
+"""
+subprocess.call('git status', shell=True)
+subprocess.call(['tree', '/f'])
+"""
+# Be careful about using shell = True
+"""
+args = ["echo", "realtime abc"]
+p = Popen(args, stdout=PIPE, stderr=PIPE, shell=True, text=True)
+
+for line in p.stdout:
+    print("O=:", line)
+"""
+"""
 class RunSqlScripts:
 
 if __name__ == "__main__":
+"""
