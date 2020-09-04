48de618656a8c738780370ab244fa0f1d55fa606
tieje <toj320@gmail.com>
2020-09-04 00:41:00 -0400

    Completed designbugreport.py. I also finally used .gitignore for the first time. So that was cool.

--- designbugreport.py
+++ designbugreport.py
@@ -1,76 +1,114 @@
 import os
 import xlwings as xw
-
-class DesignBugReport:
-	def __init__(self, default):
-		self.default = default
-		self.prompt = '>'
-	def controlla(self):
-		folder_finder()
-		excel_finder()
-	def folder_finder(self):
-		for i in os.listdir():
-			print(i)
-		print("In which folder would you like to design the bug report?")
-		self.working_dir = input(self.prompt)
-		os.chdir(os.path.join(self.default,self.working_dir))
-	def excel_finder(self):
-		xw.sheets
-	def design_excel(self):
-		# Delete the first three rows
-		sht.range('1:3').api.Delete(DeleteShiftDirection.xlShiftToLeft)
-		# going down first
-		# counterA find the row that contains MPCORE-167
-		counterA = 1
-		# counterB finds number of filled rows
-		counterB = 1
-		cut_off = 'MPCORE-167'
-		trigger = True
-		cell_value = sht.range('A2').value
-		while bool(cell_value) != False:
-			if trigger:
-				if cell_value == cut_off:
-					trigger = False
-				status_checker(counterA)
-				counterA += 1
-			counterB += 1
-			cell_value = sht.range('A'+counterB).value
-		# Delete the rows under MPCORE-167
-		sht.range(counter':'self.rows).api.Delete(DeleteShiftDirection.xlShiftToLeft)
-		# Change column names
-		cell_value = sht.range('A1').value
-		counter = 1
-# TODO: Build the status_checker that will highlight the entire row to be the color corresponding to status.
-	def status_checker(self):
-
-
-if __name__ == "__main__":
-	default = r"C:\WebTesting_Jira\Reports"
-	#TODO: I'll need to confirm with the user to determine which is the new bug report and which is the old bug report
-	#TODO: Ask the user if the current default location correct, print out the default. If not then it will ask the user to change the default path variable
-	DesignBugReport(default)
-
-"""
-testing below
-"""
-import xlwings as xw
 from xlwings.constants import DeleteShiftDirection
 
-app = xw.App()
-wb = app.books.open('name.xlsx')
-sht = wb.sheets['Sheet1']
+class DesignBugReport:
+    def __init__(self, default):
+        self.default = default
+        self.copy_counter = 0
+        self.normal_statuses = ['Backlog',
+            'Requirements',
+            'Selected for Development',
+            'In Progress',
+            'Code Review',
+            'Ready for Test',
+            'Testing',
+            'Demo',
+            'Done']
+    def controlla(self):
+        self.excel_finder()
+        self.design_excel()
+        return None
+    def excel_finder(self):
+        os.chdir(default)
+        for i in os.listdir():
+            print(i)
+        print("In which folder would you like to design the bug report?")
+        self.working_dir = os.path.join(self.default,input(prompt))
+        os.chdir(self.working_dir)
+        dir_list = os.listdir()
+        for x in dir_list:
+            if 'old' in x.lower():
+                self.copy_book = xw.Book(x)
+            elif 'new' in x.lower():
+                self.work_book = xw.Book(x)
+        self.sht = self.work_book.sheets[0]
+        self.copy_sht = self.copy_book = self.copy_book.sheets[0]
+        return None
+    def design_excel(self):
+        # Delete the first three rows
+        if self.sht.range('A1').value != 'Key':
+            self.sht.range('1:3').api.Delete(DeleteShiftDirection.xlShiftToLeft)
+        # go across first to establish labels for columns
+        i = 65
+        cell_value = self.sht.range(chr(i)+'1').value
+        while bool(cell_value):
+            if cell_value.lower() == 'status':
+                self.status_col = chr(i)
+            if cell_value == 'External Tracking #':
+                self.sht.range(chr(i)+'1').value = 'External Tracking # (for client use)'
+            i += 1
+            cell_value = self.sht.range(chr(i)+'1').value
+        self.right_limit=chr(i)
+        # checking for status column of old excel sheet
+        d = 65
+        cell_value = self.copy_sht.range(chr(d)+'1').value
+        while bool(cell_value):
+            if cell_value.lower() == 'status':
+                self.old_status_col = chr(d)
+            d += 1
+            cell_value = self.copy_sht.range(chr(d)+'1').value
+        self.old_right_limit=chr(d)
+        # going down second
+        # counterA find the row that contains MPCORE-167
+        counterA = 1
+        # counterB finds number of filled rows
+        counterB = 1
+        cut_off = 'MPCORE-167'
+        trigger = True
+        cell_value = self.sht.range('A2').value
+        while bool(cell_value) != False:
+            if trigger:
+                if cell_value == cut_off:
+                    trigger = False
+                self.status_checker(str(counterA))
+                counterA += 1
+            counterB += 1
+            cell_value = self.sht.range('A'+str(counterB)).value
+        # Delete the rows under MPCORE-167
+        self.sht.range(str(counterA)+':'+str(counterB)).api.Delete(DeleteShiftDirection.xlShiftToLeft)
+        # copy status from old excel
+        cell_value = self.sht.range('A2').value
+        counterA = 1
+        while bool(cell_value):
+            self.copy_excel(counterA)
+            counterA += 1
+            cell_value = self.sht.range('A'+str(counterA)).value
+        return None
+    def status_checker(self, row_n):
+        cell_val = self.sht.range(self.status_col+row_n).value
+        if cell_val == 'Requirements':
+            # Blue color is RGB of 55,70,150 respectively
+            self.sht.range('A'+row_n+':'+self.right_limit+row_n).color = (135,206,235)
+        elif cell_val == 'In Progress':
+            # yellow color is RGB of 255,220,50
+            self.sht.range('A'+row_n+':'+self.right_limit+row_n).color = (255,220,50)
+        elif cell_val == 'Done':
+            self.sht.range('A'+row_n+':'+self.right_limit+row_n).color = (124,252,0)
+        return None
+    def copy_excel(self, row_n):
+        while self.copy_sht.range('A'+str(row_n)).value != self.sht.range('A'+str(row_n+self.copy_counter)).value:
+            self.copy_counter += 1
+        if self.copy_sht.range(self.old_status_col+str(row_n)).value not in self.normal_statuses:
+            self.sht.range(self.status_col+str(row_n+self.copy_counter)).value = self.copy_sht.range(self.old_status_col+str(row_n)).value
+        return None
 
-# Delete row 2
-sht.range('2:2').api.Delete(DeleteShiftDirection.xlShiftUp) 
-
-# Delete row 2, 3 and 4 
-sht.range('2:4').api.Delete(DeleteShiftDirection.xlShiftUp) 
-
-# Delete Column A
-sht.range('A:A').api.Delete(DeleteShiftDirection.xlShiftToLeft)
-
-# Delete Column A, B and C
-sht.range('A:C').api.Delete(DeleteShiftDirection.xlShiftToLeft)
-
-wb.save()
-app.kill()
+if __name__ == "__main__":
+    default = r"C:\WebTesting_Jira\Reports"
+    prompt = '>'
+    print("Is the following the correct default path? If yes, then press enter. If not, then press Ctrl + C to cancel the script and go change the default.")
+    print(default)
+    response = input(prompt)
+    if bool(response) == False:
+        pass
+    DesignBugReport(default).controlla()
--- read_mes/designbugreport_readme.txt
+++ read_mes/designbugreport_readme.txt
@@ -1,6 +1,7 @@
 - python 3.7
 - pip install xlwings
 - change the "default" variable to be the path of the folder that contains the folders for company-specific bug reports
+- will not work correctly if there are more than 26 columns
 
 Steps for using this script
 1. Export the issue report from excel with the correct three-letter company label and the following columns included:
@@ -14,5 +15,32 @@ Steps for using this script
 	- Resolution
 	- Status
 	- Summary
-Use these as your default columns.
-2. 
+    Use these as your default columns.
+
+2. Save the export as an .xlsx file in your company-specific default folder:
+	Ex.
+	default is C:\WebTesting_Jira\Reports
+	company-specific default is C:\WebTesting_Jira\Reports\KPC
+
+3. Update your company-specific default folder from SVN (subversion)
+
+4. Rename your reports in the following way:
+    NEW_[COMPANY NAME]_BUG_REPORT_09_03_2020
+    OLD_[same as above, just put the "OLD" as the first word]
+    Putting the New and Old in front will be important.
+
+5. Run the script: designbugreport.py
+
+6. Commit changes to SVN.
+
+
+Instructions for BRMs
+
+1. Please checkout the company-specific folder from SVN.
+	In this case they will be located in the following location in SVN:
+	svn:https://mpsvn.medhokapps.com/cps/CPS/_Sandbox/ThomasF/WebTesting_Jira/Reports/KPC
+
+2. If the BRM wants to make changes do the following:
+	a. Update from SVN.
+	b. Make changes to the "Status" column only.
+	c. Commit changes to SVN.
